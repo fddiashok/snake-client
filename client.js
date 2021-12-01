fa65f7cc1c7ee0359672a6f0e3c4028e30113982
fddiashok <fddiashok@gmail.com>
2021-12-01 15:47:01 -0500

    added snake initials

--- client.js
+++ client.js
@@ -4,10 +4,24 @@ const connect = function () {
     host: '165.227.47.243',
     port: '50541'
   });
+
+// event handler to let the client know that connection has been setup wwith server
+conn.on('connect',()=>{
+console.log("Successfully connected to game server");
+});
+
+
+conn.on('connect', () => {
+  conn.write("Name: AK");
+});
+
+
 //event handler to get data from server
- conn.on('data',(data)=>{
-   console.log(data);
- });
+conn.on('data',(data)=>{
+  console.log(data);
+});
+
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
